9d0eaf6d32c7f79f57b2dcb52e3044e1b848a241
Unai Motriko <unaimotriko@gmail.com>
2017-06-18 19:54:09 +0100

    Initial Commit

---
+++ Asteroids-gameTests/Asteroids_gameTests.swift
@@ -0,0 +1,36 @@
+//
+//  Asteroids_gameTests.swift
+//  Asteroids-gameTests
+//
+//  Created by Unai Motriko on 18/06/2017.
+//  Copyright © 2017 Unai Motriko. All rights reserved.
+//
+
+import XCTest
+@testable import Asteroids_game
+
+class Asteroids_gameTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
